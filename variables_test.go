263e1e88df69862f8ff6e286f474f4ae986b4a47
Michael Quigley <michael@quigley.com>
2022-02-21 14:46:22 -0500

    test for multiple variables in data (#6)

--- variables_test.go
+++ variables_test.go
@@ -5,11 +5,37 @@ import (
 	"testing"
 )
 
-func TestVariableReference(t *testing.T) {
+func Test_VariableReference(t *testing.T) {
 	v, found := variableReference("${oh.wow}")
 	assert.True(t, found)
 	assert.Equal(t, "oh.wow", v)
 
 	v, found = variableReference(int16(1))
 	assert.False(t, found)
+}
+
+func Test_Variables_Multiple(t *testing.T) {
+	type simple struct {
+		Name string
+	}
+	obj := &simple{}
+	data := map[string]interface{}{
+		"name": "name:${a}/${b}/${c}",
+	}
+	opt := DefaultOptions()
+	opt.AddVariableResolver(func(name string) (interface{}, bool) {
+		switch name {
+		case "a":
+			return "AAA", true
+		case "b":
+			return "BBB", true
+		case "c":
+			return "CCC", true
+		default:
+			return "", false
+		}
+	})
+	err := Bind(obj, data, opt)
+	assert.NoError(t, err)
+	assert.Equal(t, "name:AAA/BBB/CCC", obj.Name)
 }
